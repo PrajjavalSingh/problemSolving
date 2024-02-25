08b01176660bd38347f227c46db1d8b4e9afcba7
Prajjaval Singh <prajjavalsingh@gmail.com>
2024-02-26 01:12:16 +0530

    DFS

---
+++ DFS.cc
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int maxDepth(TreeNode* root) 
+    {
+        if ( !root )
+            return 0;
+
+        int depth_left = maxDepth( root->left );
+        int depth_right = maxDepth( root->right );
+        return 1 + max( depth_right, depth_left );
+    }
+};
