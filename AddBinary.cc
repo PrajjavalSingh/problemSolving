ca6645f48abe75f64c6273e55c57d9a4ce236a47
Prajjaval Singh <prajjavalsingh@gmail.com>
2024-03-03 21:27:25 +0530

    added solution to add binary numbers

---
+++ AddBinary.cc
@@ -0,0 +1,69 @@
+//https://leetcode.com/problems/add-binary/submissions/1192766724/?envType=study-plan-v2&envId=top-interview-150i
+//
+//
+class Solution {
+public:
+    string addBinary(string a, string b) 
+    {
+        const int asz = a.size();
+        const int bsz = b.size();
+        if ( asz != bsz )
+        {
+            if ( asz > bsz )
+            {
+                const int nrplaces = asz - bsz;
+                for ( int idx=0; idx<nrplaces; idx++ )
+                {
+                    b.insert(0,"0");
+                }
+            }
+            else
+            {
+                const int nrplaces = bsz - asz;
+                for ( int idx=0; idx<nrplaces; idx++ )
+                {
+                    a.insert(0,"0");
+                }
+            }
+        }
+
+        const int sz = a.size();
+        stack<int,vector<int>> ret;
+        bool hascarry = false;
+        for ( int idx=sz-1; idx>=0; idx-- )
+        {
+            char ch1 = a[idx];
+            char ch2 = b[idx];
+            const int a_idx = ch1 == '0' ? 0 : 1;
+            const int b_idx = ch2 == '0' ? 0 : 1;
+            int c = a_idx ^ b_idx;
+            if ( hascarry )
+                c = c ^ 1;
+
+            if ( a_idx & b_idx )
+            {
+                hascarry = true;
+            }
+            else if ( (a_idx || b_idx) && hascarry )
+             {
+                hascarry = true;
+             }
+            else
+                hascarry = false;
+
+            ret.push( c );
+        }
+
+        if ( hascarry )
+            ret.push(1);
+
+        string retstr;
+        while( ret.size() )
+        {
+            retstr += to_string( ret.top() );
+            ret.pop();
+        }
+
+        return retstr;
+    }
+};
