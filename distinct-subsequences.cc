01369e8248dce7ebbb8eb3285a3e55883a0a3172
Prajjaval Singh <prajjavalsingh@gmail.com>
2024-03-23 22:01:11 +0530

    ditinct subsequences

---
+++ distinct-subsequences.cc
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/distinct-subsequences/
+
+class Solution {
+public:
+    void helper( int& ans, const string& s, const string& t, string& curr, int startidx )
+    {
+        if ( curr == t )
+        {
+            ans++;
+            return;
+        }
+
+        for ( int idx=startidx; idx<s.size(); idx++ )
+        {
+            char ch = s[idx];
+            curr.push_back(ch);
+            helper( ans, s, t, curr, idx+1 );
+            curr.pop_back();
+        }
+    }
+
+
+    int numDistinct(string s, string t) 
+    {
+        int ans = 0;
+        string curr;
+        helper( ans, s, t, curr, 0 );
+        return ans;
+    }
+};
